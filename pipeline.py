73977d0792b73b763657ebe4daf4ec6cab68ecc7
dev <dev007mail@gmail.com>
2022-07-14 15:55:07 +0530

    pipline

---
+++ pipeline.py
@@ -0,0 +1,28 @@
+import datetime
+
+import pendulum
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
+
+with DAG(
+    dag_id='pipeline for bash operator',
+    schedule_interval='@daily',
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False,
+    description='Hello World DAG pipline'
+   
+    
+) as dag:
+
+        task1=BashOperator(
+            task_id="task1",
+            bash_command="echo task 1"
+        )
+
+        task2=BashOperator(
+            task_id="task2",
+            bash_command="echo task 2"
+        )
+        task1.set_downstream(task2)
