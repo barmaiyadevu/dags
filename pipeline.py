dedcea75d92a411ed37d22301e530366ca6cb041
dev <dev007mail@gmail.com>
2022-07-15 14:03:27 +0530

    pipline updated

--- pipeline.py
+++ pipeline.py
@@ -6,6 +6,7 @@ from airflow.contrib.operators.spark_submit_operator import SparkSubmitOperator
 #*****************DAG section************************************************************88
 default_args = {
     'owner': 'devesh barmaiya',
+    'start_date': datetime(2022, 11, 18),
     'retries': 1,
 	  'retry_delay': timedelta(hours=1)
 }
