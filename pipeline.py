aa6ca443cf3e6b8ecaa32acdfee7f49ba71b65bd
dev <dev007mail@gmail.com>
2022-07-15 12:15:05 +0530

    pipline updated

--- pipeline.py
+++ pipeline.py
@@ -1,12 +1,11 @@
 from datetime import datetime, timedelta
-
-
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash import BashOperator
 from airflow.operators.empty import EmptyOperator
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
+from airflow.contrib.operators.spark_submit_operator import SparkSubmitOperator
 
 # spark code section
 def processo_etl_spark():
@@ -32,15 +31,20 @@ default_args = {
     'retries': 10,
 	  'retry_delay': timedelta(hours=1)
 }
-with DAG('dag_teste_spark_documento_vencido_v01',
+dag_spark =DAG('dag_teste_spark_documento_vencido_v01',
                   default_args=default_args,
-                  schedule_interval='0 1 * * *') as dag:
+                  schedule_interval='0 1 * * *') 
 
-    task_elt_documento_pagar = PythonOperator(
-        task_id='elt_documento_pagar_spark',
-        python_callable=processo_etl_spark
-    )
+spark_submit_local = SparkSubmitOperator(
+		application ='fileread.py' ,
+		conn_id= 'spark_local', 
+		task_id='spark_submit_task', 
+		dag=dag_spark
+		)
 
-    task_elt_documento_pagar
+spark_submit_local
+
+if __name__ == "__main__":
+    dag_spark.cli()
 
    #*********************************************************************************     
