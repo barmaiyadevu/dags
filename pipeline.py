082ca19aede5fbb26a54f19f93d0b72e418e6c22
dev <dev007mail@gmail.com>
2022-07-15 13:59:35 +0530

    pipline updated

--- pipeline.py
+++ pipeline.py
@@ -5,18 +5,25 @@ from airflow.contrib.operators.spark_submit_operator import SparkSubmitOperator
 
 #*****************DAG section************************************************************88
 default_args = {
-    'owner': 'jozimar',
-    'start_date': datetime(2020, 11, 18),
-    'retries': 10,
+    'owner': 'devesh barmaiya',
+    'retries': 1,
 	  'retry_delay': timedelta(hours=1)
 }
-dag_spark =DAG('sparksubmit_v02',
+dag_spark =DAG('sparksubmit_v03',
                   default_args=default_args,
-                  schedule_interval='0 12 * * *') 
+                  schedule_interval='* * * * *') 
 
+# IMPORTATNT spark_local connection define in airflow connection  to run local
+
+# connection id : spark_local
+# host : local[*]
+# exra : {"queue","default", "master":"local[*]", "spark-home":"/opt/spark/spark-3.1-bin-hadoop2.7",
+# "spark-binary":"spark-submit", "namespace":"default"
+# }
 spark_submit_local = SparkSubmitOperator(
 		application ='fileread.py' ,
-		conn_id= 'spark_default', 
+		conn_id= 'spark_local', 
+
 		task_id='spark_submit_task', 
 		dag=dag_spark
 		)
